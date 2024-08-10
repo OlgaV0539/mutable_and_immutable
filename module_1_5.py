8f15089227f8650ed7be7b090778ab07907cbbbc
OlgaV0539 <olv-personal@mail.ru>
2024-08-10 12:40:54 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 6, 7, 'Rose', True
+print(immutable_var)
+# immutable_var [2] = 'Nose' # Ошибка - неизменяемый объект
+mutable_list = [7, 8, 'Stone', True]
+print(mutable_list)
+mutable_list [3] = False
+print(mutable_list)
